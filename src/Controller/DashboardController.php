f318a68d1e2f50be7a26b3087cf08c0e98dc9393
Dylan Jennings <dylan.jennings077@gmail.com>
2021-04-20 19:15:43 +0100

    Added charts and made scaling changes

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,9 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
-use function Symfony\Component\String\s;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractController
 {
@@ -63,7 +62,7 @@ class DashboardController extends AbstractController
      * @Route("/log", name="app_shift_log")
      * @return Response
      */
-    public function shiftLog(): Response
+    public function shiftLog(ChartBuilderInterface $chartBuilder): Response
     {
         $result = [];
 
@@ -80,8 +79,32 @@ class DashboardController extends AbstractController
             $result[$key]['duration'] = $interval->format('%h Hours %i Minutes');
         }
 
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+        $chart->setData([
+            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            'datasets' => [
+                [
+                    'label' => 'Sales!',
+                    'backgroundColor' => 'rgb(255, 99, 132)',
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [522, 1500, 2250, 2197, 2345, 3122, 3099],
+                ],
+            ],
+        ]);
+        $chart->setOptions([
+            'scales' => [
+                'yAxes' => [[
+                    'ticks' => [
+                        'beginAtZero' => true
+                    ]
+                ]]
+            ]
+        ]);
+
+
         return $this->render('dashboard/shiftlog.html.twig', [
             'shiftHistory' => $result,
+            'chart' => $chart
         ]);
     }
 }
