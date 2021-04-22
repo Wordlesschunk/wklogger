3a50523b9cdb9109d6ddbf7082be87af3baca4cf
Dylan Jennings <dylan.jennings077@gmail.com>
2021-04-22 18:24:53 +0100

    Added icons

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+
 // start the Stimulus application
 import './bootstrap';
 
