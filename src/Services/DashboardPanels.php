f7dc8e75b0038c2a4d4bbe6d3a0ae917aa346475
Dylan Jennings <dylan.jennings077@gmail.com>
2021-04-28 21:41:20 +0100

    updated panel logic

--- src/Services/DashboardPanels.php
+++ src/Services/DashboardPanels.php
@@ -72,8 +72,11 @@ class DashboardPanels
             $totalSeconds += $totalDuration;
         }
 
+        $init = $totalSeconds;
+        $hours = floor($init / 3600);
+        $minutes = floor(($init / 60) % 60);
 
-        return Carbon::parse($totalSeconds)->format( 'H:i:s');
+        return sprintf('%d Hours %s Minutes', $hours, $minutes);
     }
 
     /**
